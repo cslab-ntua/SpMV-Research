c3a5d6437178bd3d23ff2cd0d74b53a2167730b5
Panagiotis Mpakos <pmpakos@cslab.ece.ntua.gr>
2021-11-26 02:18:01 +0200

    fpga update

--- FPGA_stuff/bash_scripts/untitled.sh
+++ FPGA_stuff/bash_scripts/untitled.sh
@@ -1,4 +1,4 @@
-export ID=1; export RANGE="512-1024"; time python3 python/gen_signature_synthetic.py --partition --mtx_param_list $REST_PREFIX/normal_$RANGE_$ID.txt --sig_path ./sig_dat --vec_path ./vec_dat 2>&1 | tee $REST_PREFIX/progress_$RANGE_$ID
+export ID=0; export RANGE="512-1024"; time python3 python_exp/gen_signature_synthetic.py --partition --mtx_param_list $REST_PREFIX/normal_$RANGE_$ID.txt --sig_path ./sig_dat --vec_path ./vec_dat 2>&1 | tee $REST_PREFIX/progress_$RANGE_$ID
 
  export MATRIX="
 "; mv $OLD_PREFIX/sig_dat/$MATRIX $NEW_PREFIX/sig_dat; mv $OLD_PREFIX/vec_dat/$MATRIX $NEW_PREFIX/vec_dat
