44f364f0dbc89faadfc603995246962555719ff1
pmpakos <pmpakos@hotmail.com>
2021-12-10 23:32:50 +0200

    remove duplicates from generation of validation matrix friends

--- Pythonia/local_ipynb/create_validation_friends_parameters.py
+++ Pythonia/local_ipynb/create_validation_friends_parameters.py
@@ -13,8 +13,14 @@ name = webbase-1M
 9) seed = 14
 '''
 def create_param_file(param_file, scaling_list):
+    vm_features = pd.read_csv("../../Benchmarks/validation_matrices_features.csv")
+    mtx_names = list(vm_features["matrix"])
+    vm_features['skew_coeff'] = (vm_features['nnz-r-max'] - vm_features['nnz-r-avg']) / vm_features['nnz-r-avg']
+    vm_features = vm_features[['matrix','nr_rows','nr_cols', 'nnz-r-avg', 'nnz-r-std', 'bw-avg', 'skew_coeff']]
+
     cnt = 0
     fw = open(param_file,"w")
+    param_list = []
     for matrix in mtx_names:
         for index, curr in vm_features[vm_features["matrix"] == matrix].iterrows():
             # print(matrix)
@@ -41,17 +47,16 @@ def create_param_file(param_file, scaling_list):
                             for placement in placement_list:
                                 # print(nr_rows, nr_cols, avg_nnz_per_row, std_nnz_per_row, distribution, placement, avg_bw, skew_coeff, seed)
                                 param = " ".join(str(x) for x in [nr_rows, nr_cols, avg_nnz_per_row, std_nnz_per_row, distribution, placement, avg_bw, skew_coeff, seed])
-                                fw.write(param+"\n")
-                                cnt+=1
+                                if(param not in param_list):
+                                    param_list.append(param)
+                                    fw.write(param+"\n")
+                                    cnt+=1
+                                else:
+                                    print("PREVENTED DUPLICATE ENTRY OF", param)
     fw.close()
     print(param_file, '\t', cnt)
 
 if __name__ == '__main__':
-    vm_features = pd.read_csv("../../Benchmarks/validation_matrices_features.csv")
-    mtx_names = list(vm_features["matrix"])
-    vm_features['skew_coeff'] = (vm_features['nnz-r-max'] - vm_features['nnz-r-avg']) / vm_features['nnz-r-avg']
-    vm_features = vm_features[['matrix','nr_rows','nr_cols', 'nnz-r-avg', 'nnz-r-std', 'bw-avg', 'skew_coeff']]
-
     param_file_small = "validation_matrices_friends_small.txt"
     scaling_list_small = [0.8, 1.2]
     create_param_file(param_file_small, scaling_list_small)
