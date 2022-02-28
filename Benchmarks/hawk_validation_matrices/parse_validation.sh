af23389b3a9e53cc304895428317cb65203b073d
Dimitris Galanopoulos <dimgal9@gmail.com>
2022-02-28 13:12:58 +0200

    hawk change mkl names

--- Benchmarks/hawk_validation_matrices/parse_validation.sh
+++ Benchmarks/hawk_validation_matrices/parse_validation.sh
@@ -5,12 +5,13 @@ bench_dir='../'
 val_mat_dir="./"
 
 data=(
-    'csr5_d.out             CSR5_CPU'
-    'csr_custom_BV_d.txt    Custom_CSR_BV_CPU'
-    'csr_custom_B_d.txt     Custom_CSR_B_CPU'
-    'csr_naive_d.out        Naive_CSR_CPU'
-    'mkl_ie_d.out           MKL_IE'
-    'mkl_ie_opt_d.out       MKL_IE_Optimize'
+    'csr5_d.out                     CSR5_CPU'
+    'csr_custom_BV_d.txt            Custom_CSR_BV_CPU'
+    'csr_custom_B_d.txt             Custom_CSR_B_CPU'
+    'csr_naive_d.out                Naive_CSR_CPU'
+    'mkl_ie_d.out                   MKL_IE_no_optimize'
+    'mkl_ie_opt_d.out               MKL_IE'
+    'mkl_ie_opt_no_hint_d.out       MKL_IE_no_hint'
 )
 
 
